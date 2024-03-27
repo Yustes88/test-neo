da5859fa1b2935892ab7ea9fd51214aaefe05c45
Yustes88 <iuliiaobr.io@gmail.com>
2024-03-27 20:08:51 +0300

    feat:added parked pages

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,18 +1,17 @@
-import "./styles.css";
-import "../../App.css";
-import Logo from "../ui/Logo";
-import { Link } from "react-router-dom";
-import { icons } from "../../data/data";
-import { type FC } from "react";
-import { useCartContext } from "../../providers/CartProvider";
-import IconLink from "../ui/IconButton";
+import './styles.css'
+import '../../App.css'
+import Logo from '../ui/Logo'
+import {icons} from '../../data/data'
+import {type FC} from 'react'
+import {useCartContext} from '../../providers/CartProvider'
+import IconLink from '../ui/IconButton'
 
 const Nav: FC = () => {
-  const { cartItems } = useCartContext();
+  const {cartItems} = useCartContext()
 
   const total = cartItems.reduce((item1, item2) => {
-    return item1 + item2.total;
-  }, 0);
+    return item1 + item2.total
+  }, 0)
 
   return (
     <div className="container">
@@ -20,11 +19,11 @@ const Nav: FC = () => {
         <Logo />
         <div>
           <ul className="icons-list">
-            {icons.map((icon) => (
-              <IconLink toPath={icon.src} className={"icon-link"} key={icon.id}>
+            {icons.map(icon => (
+              <IconLink toPath={icon.src} className={'icon-link'} key={icon.id}>
                 {icon.icon}
                 <span className="icon-badge">
-                  {icon.key === "cart" ? total : 0}
+                  {icon.key === 'cart' ? total : 0}
                 </span>
               </IconLink>
             ))}
@@ -32,7 +31,7 @@ const Nav: FC = () => {
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Nav;
+export default Nav
--- src/data/data.tsx
+++ src/data/data.tsx
@@ -1,144 +1,145 @@
-import { LuHeart } from "react-icons/lu";
-import { MdOutlineShoppingCart } from "react-icons/md";
-import { v4 as uuidv4 } from "uuid";
+import {LuHeart} from 'react-icons/lu'
+import {MdOutlineShoppingCart} from 'react-icons/md'
+import {v4 as uuidv4} from 'uuid'
 import {
   type Route,
   type FooterLink,
   type Product,
   type SocialLink,
   type Language,
-} from "../types/types";
-import ProductsList from "../components/products-list/ProductsList";
-import CartList from "../components/cart-list/CartList";
+} from '../types/types'
+import ProductsList from '../components/products-list/ProductsList'
+import CartList from '../components/cart-list/CartList'
+import Parked from '../components/parked/Parked'
 
 export const footerLinks: FooterLink[] = [
   {
     id: uuidv4(),
-    title: "Избранное",
-    url: "/",
+    title: 'Избранное',
+    url: '/favourites',
   },
   {
     id: uuidv4(),
-    title: "Корзина",
-    url: "/",
+    title: 'Корзина',
+    url: '/cart',
   },
   {
     id: uuidv4(),
-    title: "Контакты",
-    url: "/",
+    title: 'Контакты',
+    url: '/contacts',
   },
-];
+]
 
 export const headphones: Product[] = [
   {
     id: uuidv4(),
-    title: "Apple BYZ S852I",
+    title: 'Apple BYZ S852I',
     newPrice: 2927,
     oldPrice: 3629,
     rate: 4.7,
-    src: "/products/product-1.png",
+    src: '/products/product-1.png',
   },
   {
     id: uuidv4(),
-    title: "Apple EarPods",
+    title: 'Apple EarPods',
     newPrice: 2327,
     rate: 4.5,
-    src: "/products/product-2.png",
+    src: '/products/product-2.png',
   },
   {
     id: uuidv4(),
-    title: "Apple EarPods",
+    title: 'Apple EarPods',
     newPrice: 2327,
     rate: 4.5,
-    src: "/products/product-3.png",
+    src: '/products/product-3.png',
   },
   {
     id: uuidv4(),
-    title: "Apple EarPods",
+    title: 'Apple EarPods',
     newPrice: 2327,
     rate: 4.5,
-    src: "/products/product-1.png",
+    src: '/products/product-1.png',
   },
   {
     id: uuidv4(),
-    title: "Apple BYZ S852I",
+    title: 'Apple BYZ S852I',
     newPrice: 2927,
     oldPrice: 3629,
     rate: 4.7,
-    src: "/products/product-2.png",
+    src: '/products/product-2.png',
   },
   {
     id: uuidv4(),
-    title: "Apple EarPods",
+    title: 'Apple EarPods',
     newPrice: 2327,
     rate: 4.5,
-    src: "/products/product-3.png",
+    src: '/products/product-3.png',
   },
-];
+]
 
 export const wirelessHeadphones: Product[] = [
   {
     id: uuidv4(),
-    title: "Apple AirPodsI",
+    title: 'Apple AirPodsI',
     newPrice: 9257,
     rate: 4.7,
-    src: "/products/wireless-1.png",
+    src: '/products/wireless-1.png',
   },
   {
     id: uuidv4(),
-    title: "GERLAX GH-04",
+    title: 'GERLAX GH-04',
     newPrice: 6527,
     rate: 4.5,
-    src: "/products/wireless-2.png",
+    src: '/products/wireless-2.png',
   },
   {
     id: uuidv4(),
-    title: "BOROFONE BO4",
+    title: 'BOROFONE BO4',
     newPrice: 7527,
     rate: 4.5,
-    src: "/products/wireless-3.png",
+    src: '/products/wireless-3.png',
   },
-];
+]
 
 export const icons = [
   {
     id: uuidv4(),
-    src: "/cart",
-    key: "cart",
+    src: '/cart',
+    key: 'cart',
     icon: <MdOutlineShoppingCart color="#838383" size={22} />,
     total: 0,
   },
   {
     id: uuidv4(),
-    src: "/",
-    key: "favourites",
+    src: '/favourites',
+    key: 'favourites',
     icon: <LuHeart color="#838383" size={22} />,
     total: 0,
   },
-];
+]
 
 export const languages: Language[] = [
   {
     id: uuidv4(),
-    title: "Ru",
-    url: "/",
+    title: 'Ru',
+    url: '/',
   },
   {
     id: uuidv4(),
-    title: "Eng",
-    url: "/eng",
+    title: 'Eng',
+    url: '/eng',
   },
-];
+]
 
 export const routes: Route[] = [
   {
     id: uuidv4(),
-    path: "/",
+    path: '/',
     component: (
       <>
-        <ProductsList title={"Наушники"} products={headphones} />
+        <ProductsList title={'Наушники'} products={headphones} />
         <ProductsList
-          title={"Беспроводные наушники"}
+          title={'Беспроводные наушники'}
           products={wirelessHeadphones}
         />
       </>
@@ -146,38 +147,48 @@ export const routes: Route[] = [
   },
   {
     id: uuidv4(),
-    path: "/cart",
+    path: '/cart',
     component: <CartList />,
   },
   {
     id: uuidv4(),
-    path: "/eng",
+    path: '/eng',
     component: (
       <>
-        <ProductsList title={"Headphones"} products={headphones} />
+        <ProductsList title={'Headphones'} products={headphones} />
         <ProductsList
-          title={"Wireless headphones"}
+          title={'Wireless headphones'}
           products={wirelessHeadphones}
         />
       </>
     ),
   },
-];
+  {
+    id: uuidv4(),
+    path: '/favourites',
+    component: <Parked title={'favourites'} />,
+  },
+  {
+    id: uuidv4(),
+    path: '/contacts',
+    component: <Parked title={'contacts'} />,
+  },
+]
 
 export const socialMediaLinks: SocialLink[] = [
   {
     id: uuidv4(),
-    url: "https://vk.com",
-    icon: "/socials/Vk.svg",
+    url: 'https://vk.com',
+    icon: '/socials/Vk.svg',
   },
   {
     id: uuidv4(),
-    url: "https://telegram.org/",
-    icon: "/socials/Telegram.svg",
+    url: 'https://telegram.org/',
+    icon: '/socials/Telegram.svg',
   },
   {
     id: uuidv4(),
-    url: "https://web.whatsapp.com/",
-    icon: "/socials/Whatsapp.svg",
+    url: 'https://web.whatsapp.com/',
+    icon: '/socials/Whatsapp.svg',
   },
-];
+]
